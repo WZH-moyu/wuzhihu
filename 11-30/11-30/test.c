e26b62f50fdbe4c3eb5861c743dcfd5384def6fd
wuzhihu <445163140@qq.com>
2018-11-30 23:10:57 +0800

    杨辉三角

---
+++ 11-30/11-30/test.c
@@ -0,0 +1,69 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+//3.Ñî»ÔÈý½Ç
+//int main()
+//{
+//	int i = 0;
+//	int j = 0;
+//	int x = 0;
+//	scanf("%d", &x);
+//	int arr[50][50] = { 0 };
+//	printf("%d\n", arr[0][0] = 1);
+//	printf("\n");
+//	for (i = 1; i < x; i++)
+//	{
+//		arr[0][0] = 1;
+//		for (j = 0; j <= i; j++)
+//		{
+//			if (j == 0)
+//			{
+//				arr[i][j] = 1;
+//			}
+//			else if (i == j)
+//			{
+//				arr[i][j] = 1;
+//			}
+//			else
+//			{
+//				arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
+//			}
+//			printf("%d   ", arr[i][j]);
+//		}
+//		printf("\n");
+//		printf("\n");
+//	}
+//	system("pause");
+//	return 0;
+//}
+//
+int main()
+{
+	int A = 0;
+	int B = 0;
+	int C = 0;
+	int D = 0;
+	int E = 0;
+	for (A = 1; A <= 5; A++)
+	{
+		for (B = 1; B <= 5; B++)
+		{
+			for (C = 1; C <= 5; C++)
+			{
+				for (D = 1; D <= 5; D++)
+				{
+					for (E = 1; E <= 5; E++)
+					{
+						if (((B==2)+(A==3)))
+					}
+
+				}
+
+			}
+		}
+	
+
+	}
+	system("pause");
+	return 0;
+}

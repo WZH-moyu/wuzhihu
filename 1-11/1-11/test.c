7013dff93325ef0313b8e7a5ce586e1b5478a43a
WZH-moyu <445163140>
2019-01-24 22:38:24 +0800

    学生成绩管理

---
+++ 1-11/1-11/test.c
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+//struct BBB{
+//	long a;
+//	char b;
+//	char c;
+//	long d;
+//	long e;
+//}*p;
+//int main()
+//{
+//	p = (struct BBB*)0x100000;
+//	int x = (char*)p + 0x1;
+//	printf("0x%x", x);
+//	system("pause");
+//	return 0;
+//}
+//enum ENUM_A
+//{
+//	X1,
+//	Y1,
+//	Z1 = 255,
+//	A1,
+//	B1,
+//};
+//enum ENUM_A enumA = Y1;
+//enum ENUM_A enumB = B1;    
+//int main()
+//{
+//	printf("%d\n", enumA);
+//	printf("%d\n", enumB);
+//	system("pause");
+//	return 0;
+//}

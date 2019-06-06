8c2ae984e8f47d37f1a68fb8bb404f748571099c
WZH-moyu <445163140@qq.com>
2019-06-06 18:17:00 +0800

    完全数计算

---
+++ 6-6/6-6/test.cpp
@@ -0,0 +1,53 @@
+//#define _CRT_SECURE_NO_WARNINGS
+#include<stdlib.h>
+//#include<iostream>
+//#include<vector>
+//using namespace std;
+//int main()
+//{
+//	int num = 0;
+//	int count = 0;
+//	vector<int> v1;
+//	v1.resize(1000);
+//	int sum = 0;
+//	int n = 0;
+//	cin >> num;
+//	for (int i = 0; i <= num; i++)
+//	{
+//		for (int j = 1; j <= i/2; j++)
+//		{
+//			if (i%j == 0)
+//			{
+//				v1[n] = j;
+//				n++;
+//			}
+//			if (j == i /2)
+//			{
+//				for (int s = 0; s < n; s++)
+//				{
+//					sum += v1[s];
+//				}
+//				if (sum == i)
+//				{
+//					count++;
+//				}
+//			}
+//		}
+//		v1.clear();
+//		v1.resize(1000);
+//		n = 0;
+//		sum = 0;
+//	}
+//	cout << count << endl;
+//	system("pause");
+//	return 0;
+//}
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+	string s1;
+	string s2;
+	
+}

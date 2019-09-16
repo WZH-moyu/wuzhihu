07f7880171fbce553a6555a29b2963966e8ec1e2
WZH-moyu <445163140@qq.com>
2019-09-16 22:10:45 +0800

    快手笔试

---
+++ 9-15/9-15/test.cpp
@@ -0,0 +1,41 @@
+//#include<iostream>
+//#include<string>
+//using namespace std;
+//int main()
+//{
+//	string s;
+//	while (cin >> s)
+//	{
+//		if (s.size() >= 7 && s.size() <= 15)
+//		{
+//			cout << "IPv4" << endl;
+//			break;
+//		}
+//		else if (s.size() >= 15 && s.size() <= 37)
+//		{
+//			cout << "IPv6" << endl;
+//			break;
+//		}
+//		else
+//			cout << "Neither" << endl;
+//	}
+//	return 0;
+//}
+//#include<iostream>
+//#include<vector>
+//using namespace std;
+//int main()
+//{
+//	int num = 0;
+//	while (cin >> num)
+//	{
+//		vector<int> v;
+//		v.resize(num);
+//		for (int i = 0; i < num; i++)
+//		{
+//			cin >> v[i];
+//		}
+//		cout << num << endl;
+//	}
+//	return 0;
+//}
